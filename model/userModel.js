85cf7257f0da9095a4f49f7ebb2730e1dc2ce007
Kiyoshi Takahama <k1y05h17k@gmail.com>
2024-06-19 14:10:26 -0300

    Feat: create new folder controller to auth and user

--- model/userModel.js
+++ model/userModel.js
@@ -4,19 +4,17 @@ const bcrypt = require('bcrypt');
 
 
 const User = sequelize.define('User', {
-
-
-    id:{
+    id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
     },
-    username:{
+    username: {
         type: DataTypes.STRING,
-        allowNull:false,
-        unique:true,
-        validate:{
-            notNull:{msg: 'Please tell us your name!'}
+        allowNull: false,
+        unique: true,
+        validate: {
+            notNull: { msg: 'Please tell us your name!' }
         }
     },
     email: {
@@ -24,32 +22,64 @@ const User = sequelize.define('User', {
         allowNull: false,
         unique: true,
         lowercase: true,
-        validate:{
-            notNull:{msg: 'Please provide your email!'},
-            isEmail:{msg: 'Please provide a valid email'}
+        validate: {
+            notNull: { msg: 'Please provide your email!' },
+            isEmail: { msg: 'Please provide a valid email' }
         }
     },
-    password:{
-        type:DataTypes.STRING,
+    profileImage:{
+       type: DataTypes.STRING,
+       allowNull: true, 
+    },
+    createAt:{
+        type: DataTypes.DATE,
         allowNull: false,
-        validate:{
-            notNull:{ msg: 'Please provide a password!' },
+        defaultValue: DataTypes.NOW,
+    },
+    updateAt:{
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: null
+    },    
+    status: {
+        type: DataTypes.ENUM('active','inactive','banned'),
+        defaultValue:'active' // Add rules to the first login active the user   
+    },
+    role: {
+        type: DataTypes.ENUM('user', 'admin'),
+        defaultValue: 'user',
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notNull: { msg: 'Please provide a password!' },
             len: {
-                args:[8],
-                msg:'Password must be at least 8 characters long'
-            }  
+                args: [8],
+                msg: 'Password must be at least 8 characters long'
+            }
         }
     },
     passwordConfirm: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          notNull: { msg: 'Confirm your password' },
-          isSameAsPassword(value) {
-            if (value !== this.password) {
-              throw new Error('Passwords are not the same!');
+            notNull: { msg: 'Confirm your password' },
+            isSameAsPassword(value) {
+                if (value !== this.password) {
+                    throw new Error('Passwords are not the same!');
+                }
             }
-          }
+        }
+    }
+}, {
+    hooks: {
+        beforeSave: async (user) => {
+            if (user.password) {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+                user.passwordConfirm = undefined;
+            }
         }
     }
 });
